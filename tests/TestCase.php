5700287ef5a8de0c0728ad144a4a0592116e2c5c
Ibrahim <ibrahim.bougaoua@etu.usthb.dz>
2023-07-10 22:51:34 +0100

    fix issue

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,35 +2,11 @@
 
 namespace IbrahimBougaoua\FilamentTrace\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 use IbrahimBougaoua\FilamentTrace\FilamentTraceServiceProvider;
+use Livewire\LivewireServiceProvider;
 
 class TestCase extends Orchestra
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'IbrahimBougaoua\\FilamentTrace\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
-    protected function getPackageProviders($app)
-    {
-        return [
-            FilamentTraceServiceProvider::class,
-        ];
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_filament-trace_table.php.stub';
-        $migration->up();
-        */
-    }
+	
 }
